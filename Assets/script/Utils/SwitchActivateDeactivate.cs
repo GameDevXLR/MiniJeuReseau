8505c1be3c4ccc637210a93ef622d3c9b9cdbe30
exopole <alexandre.nicaise28@gmail.com>
2018-01-23 23:29:21 +0100

    Panel d'aide effectué

---
+++ Assets/script/Utils/SwitchActivateDeactivate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchActivateDeactivate : MonoBehaviour {
+
+    public GameObject obj1;
+    public GameObject obj2;
+
+    public void action()
+    {
+        obj1.SetActive(!obj1.activeInHierarchy);
+        obj2.SetActive(!obj2.activeInHierarchy);
+    }
+}
